5e0ecfc0fa6fd542be3cd95306244f6dac6f9c8e
HackeaMesta <rk521@hotmail.com>
2016-08-01 21:56:17 -0500

    Fix Dependencies python-webkitgtk-devel again

--- scanda/Ui.py
+++ scanda/Ui.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 import gi
 gi.require_version('Gtk', '3.0')
-gi.require_version('WebKit', '3.0')
+# only in opensuse comment this line
+#gi.require_version('WebKit', '3.0')
 import thread
 from gi.repository import Gtk
 from scanda.WebK import WebK
