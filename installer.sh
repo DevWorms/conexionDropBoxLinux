dd3a72a1b27e0840e6dfd9703f4951a9a9dabf92
HackeaMesta <rk521@hotmail.com>
2016-08-30 10:52:08 -0500

    Python 2.6 environment

--- installer.sh
+++ installer.sh
@@ -20,30 +20,3 @@ zypper rr repo-dist
 wget https://bootstrap.pypa.io/get-pip.py
 # Instala Pip
 python3.4 get-pip.py
-
-#---------------------------------------------------------------------------------------------------
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/noarch/python3-pip-7.1.2-4.5.noarch.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/noarch/python3-pyOpenSSL-0.15.1-3.3.noarch.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/noarch/python3-setuptools-18.3.2-1.1.noarch.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/noarch/readline-doc-6.2-76.4.noarch.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/expect-5.45-17.4.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/gcc-4.8-8.4.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/glibc-2.19-17.4.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/glibc-devel-2.19-17.4.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/glibc-extra-2.19-17.4.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/glibc-locale-2.19-17.4.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/libncurses5-5.9-53.4.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/libopenssl1_0_0-1.0.1i-4.1.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/libopenssl-devel-1.0.1i-4.1.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/libpython3_4m1_0-3.4.1-6.2.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/libreadline6-6.2-76.4.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/libz1-1.2.8-6.4.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/openssl-1.0.1i-4.1.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/python3-3.4.1-6.1.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/python3-base-3.4.1-6.2.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/python3-curses-3.4.1-6.1.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/python3-dbm-3.4.1-6.1.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/python3-devel-3.4.1-6.2.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/python3-tk-3.4.1-6.1.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/tcl-8.6.1-3.6.x86_64.rpm -O /tmp/dbprotector/dependencies/
-wget http://download.opensuse.org/distribution/leap/42.1/repo/oss/suse/x86_64/zlib-devel-1.2.8-6.4.x86_64.rpm -O /tmp/dbprotector/dependencies/
