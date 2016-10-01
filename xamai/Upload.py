4dd08d9f7a5a6718f306e23b52e80957609ae500
Ricardo Osorio <rk521@hotmail.com>
2016-10-01 00:36:13 -0500

    Update Dropbox api v2

--- xamai/Upload.py
+++ xamai/Upload.py
@@ -26,8 +26,34 @@ from xamai.Crypt import Crypt
 from xamai.Status import Status
 import xamai.Constants as const
 
+'''
+    Metodos usados para trabajar con la API de Dropbox
+        Estructura de carpetas en Dropbox
+            /Aplicaciones/DBProtector/user-id/año/mes/archivo
+        Estructura de carpetas en Aplicacion Dropbox
+            /user-id/año/mes/archivo
+            la Api trabaja a nivel Root de la aplicacion, ej: /1/2016/07
+        Validaciones:
+            -valida que no exista otra subida en curso
+            -Archivo existe?
+            -El tamaño del archivo debe ser menor al espacio disponible (proporcionado por la API de SCANDA)
+            -El archivo debe tener una extension valida (proporcionado por la API de SCANDA)
+            -Debe seguir un formato de nombre valido ej. LOMS9208164C520160725150501.bak LOMS920816---20160725150501.bak
+            -Si el archivo es mayor que CHUNK_SIZE (5MB) se sube por bloques, si es menor o igual se sube en una sola solicitud
+        - Return
+            - 1 = Archivo inexistente
+            - 2 = Espacio insuficiente
+            - 3 = Extension invalida
+            - None = El archivo no se subio
+            - res = JSON array con los detalles del archivo subido
+'''
+
 class Upload():
+    # Token de la cuenta
     crypt = Crypt()
+    '''
+        Para mas info leer Crypt.py
+    '''
     TOKEN = base64.b64decode(
         repr(
             crypt.obfuscate(
@@ -327,7 +353,7 @@ class Upload():
         user = self.getData()
         files = []
         dbx = dropbox.Dropbox(self.TOKEN)
-        respuesta = dbx.files_get_metadata("/" + str(user['IdCustomer']) + "/")
+        respuesta = dbx.files_list_folder("/" + str(user['IdCustomer']) + "/")
         for anio in respuesta.entries:
             meses = dbx.files_list_folder(str(anio.path_display))
             for mes in meses.entries:
@@ -561,7 +587,3 @@ class Upload():
         else:
             print "Existe otra subida en proceso"
             log.newLog(self.THIS_FILE + "." + "sync()", "error_upload_exist", "E", "")
-
-app = Upload()
-for i in app.getBackups("/3/2016/09"):
-    print i
